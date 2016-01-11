2e892a6001c901df35a9f40a104dbbd230becec1
Enric Galceran <enric.galceran@mavt.ethz.ch>
2016-01-11 10:26:02 +0100

    Minor fixes.

--- github/github_archiver.py
+++ github/github_archiver.py
@@ -19,7 +19,8 @@ try:
     from github import Github, GithubException
 except exceptions.ImportError:
     print("Unable to import from github module (PyGithub library).")
-    print("This program needs PyGithub (http://pygithub.github.io/PyGithub/v1/index.html).")
+    print("This program needs PyGithub "
+          "(http://pygithub.github.io/PyGithub/v1/index.html).")
     print("Install using (e.g.):")
     print("  easy_install PyGithub")
     print("or")
@@ -68,17 +69,24 @@ if __name__ == "__main__":
                         help='Github organization. Defaults to ethz-asl')
     parser.add_argument('-c', '--check-only', dest='check_only',
                         action='store_true',
-                        help='only check for outdated repos (no updates in last year) and print list, no further action taken (off by default)')
+                        help='only check for outdated repos (no updates in '
+                        'the last year) and print list, no further action '
+                        'taken (off by default)')
     parser.add_argument('-b', '--blacklist', dest='blacklist', type=str,
-                        help='blacklist text file with repositories to be archived, one by line (if set no check for outdated repos will be done)')
+                        help='blacklist text file with repositories to be '
+                        'archived, one by line (if set no check for outdated '
+                        'repos will be done)')
     parser.add_argument('-w', '--whitelist', dest='whitelist', type=str,
-                        help='whitelist text file with repositories to be ignored, one by line (overrides items in blacklist)')
+                        help='whitelist text file with repositories to be '
+                        'ignored, one by line (overrides items in blacklist)')
     parser.add_argument('-n', '--do-not-archive', dest='do_not_archive',
                         action='store_true',
-                        help='if set, archival of flagged repos is not performed (off by default)')
+                        help='if set, archival of flagged repos is not '
+                        'performed (off by default)')
     parser.add_argument('-d', '--delete', dest='delete',
                         action='store_true',
-                        help='delete flagged repos, will ask for confirmation (off by default)')
+                        help='delete flagged repos, will ask for confirmation '
+                        '(off by default)')
 
     args = parser.parse_args()
 
@@ -87,7 +95,8 @@ if __name__ == "__main__":
         print "Taking password from GITHUB_PASSWORD environment variable."
         password = os.environ.get("GITHUB_PASSWORD")
     else:
-        print "GITHUB_PASSWORD environment variable is not set, so will ask for password."
+        print ("GITHUB_PASSWORD environment variable is not set, so will ask "
+              "for password.")
         password = getpass.getpass("Enter password for %s: " % username)
 
     try:
@@ -102,7 +111,8 @@ if __name__ == "__main__":
             print "The following repos have not been updated in the last year:"
             for repo in all_repos:
                 if repo.updated_at < a_year_ago: # and repo.private:
-                    print "    " + repo.name + ", last updated: " + str(repo.updated_at)
+                    print "    " + repo.name + ", last updated: " + \
+                          str(repo.updated_at)
                     repo_list.append(repo.name)
 
             if args.check_only:
@@ -133,21 +143,27 @@ if __name__ == "__main__":
             repo = g.get_organization(args.organization).get_repo(r)
 
             # Clone repo
-            if os.path.exists(os.getcwd() + "/" + r):
-                print "!!! Could not clone " + r + ", name already exists in current directory."
+            if os.path.exists(os.getcwd() + "/" + r + ".git"):
+                print ("!!! Could not clone " + r + ", name already exists in "
+                       "current directory.")
             else:
-                ret = subprocess.call("git clone --mirror " + repo.ssh_url, shell=True)
+                ret = subprocess.call("git clone --mirror " + repo.ssh_url,
+                                      shell=True)
                 if ret == 0:
                     print r + " cloned successfully."
                 else:
                     print "!!! Something went wrong when cloning repo."
 
             # Clone wiki
-            # Note: looks like repo.has_wiki is always true, even if the wiki repo does not exist
-            if os.path.exists(os.getcwd() + "/" + r + ".wiki"):
-                print "!!! Could not clone " + r + "'s wiki, name already exists in current directory."
+            # Note: looks like repo.has_wiki is always true, even if the wiki
+            # repo does not exist
+            if os.path.exists(os.getcwd() + "/" + r + ".wiki.git"):
+                print ("!!! Could not clone " + r + "'s wiki, name already "
+                       "exists in current directory.")
             else:
-                ret = subprocess.call("git clone --mirror " + repo.ssh_url[:-3] + "wiki.git", shell=True)
+                ret = subprocess.call("git clone --mirror " + \
+                                      repo.ssh_url[:-3] + "wiki.git", \
+                                      shell=True)
                 if ret == 0:
                     print r + "'s wiki cloned successfully."
                 else:
@@ -156,14 +172,16 @@ if __name__ == "__main__":
             # Pickle issues
             if repo.has_issues:
                 if os.path.exists(os.getcwd() + "/" + r + ".issues"):
-                    print "!!! Could not save " + r + "'s issues, name already exists in current directory."
+                    print ("!!! Could not save " + r + "'s issues, name "
+                           "already exists in current directory.")
                 else:
                     all_issues = []
                     for issue in repo.get_issues():
                         all_issues.append(issue)
                     with open(repo.name + ".issues", 'w') as issues_file:
                         pickle.dump(all_issues, issues_file)
-                    print "Archived " + str(len(all_issues)) + " total issues for repo " + repo.name + "."
+                    print ("Archived " + str(len(all_issues)) + " total issues"
+                          " for repo " + repo.name + ".")
 
             print "\n" + r + " done.\n"
 
@@ -172,7 +190,8 @@ if __name__ == "__main__":
             print "The following repos are flagged for deletion on Github:"
             for r in repo_list:
                 sys.stdout.write(r + " ")
-            sys.stdout.write("\nDeleting a repo on Github can't be easily undone.\n")
+            sys.stdout.write("\nDeleting a repo on Github can't be easily "
+                             "undone.\n")
             answer = query_yes_no("Do you really want to delete them?")
 
             if answer:
