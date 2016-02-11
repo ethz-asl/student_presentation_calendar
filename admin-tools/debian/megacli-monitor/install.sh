20c3c60308b0c8f1fb8da57f0ab9e3293fe56dc0
HannesSommer <hannes.sommer@mavt.ethz.ch>
2016-02-11 16:59:02 +0100

    Removed megacli-monitor from the public repository because it will be run with elevated privileges

--- admin-tools/debian/megacli-monitor/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install aha colordiff realpath
-
-DIR=$(realpath $(dirname ${BASH_SOURCE[0]}))
-
-sudo ln -nsiv $DIR /root/megacli-monitor
-sudo cp -vi megacli-cron /etc/cron.d/
-
--- admin-tools/debian/megacli-monitor/update.expected.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo cp megacli-ldinfo.last.output megacli-ldinfo.expected.output
